2f5594a5bf8b4c6f55b59d62f33b39df6369d06d
Giuseppe Lo Presti <giuseppe.lopresti@cern.ch>
2023-10-31 11:58:07 +0100

    Revamp ScienceMesh integration tests (#4246)
    * Minor clarifications
    
    * Incorporated WOPI in dockerized sciencemesh testing
    
    * Added new example to run ScienceMesh integration tests against cernbox and removed unneeded files from sciencemesh example
    
    * Cleaned up spurious paths
    
    * Prepared certs for the new containers
    
    * Mock meshdir includes cernbox
    
    * Reworked wiring of reva (sm and cernbox flavours) and wopi servers + collabora
    
    * Added cernbox scenarios
    
    * Changelog
    
    * Fixes
    
    * Linting
    
    * Removed self-signed certs, they are generated at init time
    
    * Fixed references to cert files
    
    * Further tweak the wopi config for OC/NC
    
    * Fixed Collabora config
    
    * Moved to scripts
    
    * Executables
    
    * Use dedicated TLS folders also for reva
    
    * Fixes
    
    * Use trace-level debug for tests
    
    * Wired in nginx and keycloak for CERNBox to serve the OCIS web UI
    
    * Keycloak served through https
    
    * CA self-signed cert back in git
    
    * cernbox web extensions are temporarily served from a custom tgz
    
    * Minor change
    
    * Another minor change
    
    * Updated following service rename in upstream master
    
    * Also copy the browser CA DB to a scratch area as it is modified by firefox
    
    * Updated tutorial
    
    * git-ignore all testing artifacts
    
    * Fixed example
    
    * Fixed cert for mock meshdir
    
    * Serve datagateway with the hostname for the cert to match
    
    * Cleaner struct to parse JSON payload from PHP SM app
    
    * Prepared config for serving cernbox web from reva

--- init.sh
+++ init.sh
@@ -9,6 +9,16 @@ BRANCH_NEXTCLOUD_APP=nextcloud
 REPO_OWNCLOUD_APP=https://github.com/sciencemesh/nc-sciencemesh
 BRANCH_OWNCLOUD_APP=owncloud
 
+# TODO will be dropped in favour of Reva directly serving the UI
+CBOX_WEB=https://github.com/cernbox/web-release/releases/latest/download
+
+REPO_WOPISERVER=https://github.com/cs3org/wopiserver
+TAG_WOPISERVER=master
+
+# TLS folder.
+[ ! -d "tls" ] &&
+    mkdir tls && cd scripts && ./gencerts.sh && cd -
+
 # Nextcloud Sciencemesh source code.
 [ ! -d "nextcloud-sciencemesh" ] &&                                             \
     git clone                                                                   \
@@ -34,3 +44,28 @@ BRANCH_OWNCLOUD_APP=owncloud
     --workdir /var/www/html/apps/sciencemesh                                    \
     pondersource/dev-stock-owncloud-sciencemesh                                 \
     composer install
+
+# CERNBox web and extensions sources: uid=101 is nginx in the nginx container.
+# TODO the extensions are temporarily extracted from a tgz
+[ ! -d "cernbox-web-sciencemesh" ] &&                                           \
+    mkdir -p temp/cernbox-1-conf temp/cernbox-2-conf &&                         \
+    cp cernbox/nginx/* temp/cernbox-1-conf &&                                   \
+    cp cernbox/nginx/* temp/cernbox-2-conf &&                                   \
+    mkdir cernbox-web-sciencemesh &&                                            \
+    cd cernbox-web-sciencemesh &&
+    mkdir -p ./web && mkdir -p ./cernbox &&                                     \
+    wget ${CBOX_WEB}/web.tar.gz &&                                              \
+    tar xf web.tar.gz -C ./web --strip-components=1 &&                          \
+    rm -rf web.tar.gz &&                                                        \
+    tar xf ../cernbox/cernbox-extensions-bundle.tgz &&                          \
+    chmod -R 755 ./* && chown -R 101:101 ./* &&                                 \
+    cd -
+
+# wopiserver source code for the config.
+[ ! -d "wopi-sciencemesh" ] &&                                                       \
+    git clone --branch ${TAG_WOPISERVER} ${REPO_WOPISERVER} wopi-sciencemesh &&      \
+    mkdir -p temp/wopi-1-conf temp/wopi-2-conf &&                                    \
+    cp wopi-sciencemesh/wopiserver.conf temp/wopi-1-conf/wopiserver.defaults.conf && \
+    echo "shared-secret-2" > temp/wopi-1-conf/iopsecret &&                           \
+    echo "wopisecret" > temp/wopi-1-conf/wopisecret &&                               \
+    cp temp/wopi-1-conf/* temp/wopi-2-conf/
